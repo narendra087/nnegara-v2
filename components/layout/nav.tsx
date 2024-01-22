dba008eeca0544a58f0949b0748708d3839b4a2e
Narendra <personal.rendra@gmail.com>
2024-01-22 16:51:43 +0700

    [ADD] Temporary Landing Page

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,13 +1,55 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from "next/image"
+
+import { motion } from 'framer-motion'
+
 import { ModeToggle } from '../ui/toggle-mode'
+
+import Container from './container'
 
 const Nav = () => {
   return (
-    <div className='flex flex-row justify-between items-center'>
-      <p className='text-2xl font-bold'>NNEGARA V2</p>
+    <header className='flex justify-between py-3 px-4 border-b'>
+      <Container>
+        <div className='relative px-8 flex h-16 items-center justify-between w-full'>
+          <motion.div
+            initial={{
+              x: -250,
+              opacity: 0,
+            }}
+            animate={{
+              x: 0,
+              opacity: 1,
+            }}
+            transition={{
+              duration: 0.3
+            }}
+          >
+            <Link href={'/'} className='ml-0 flex flex-row gap-2 items-center'>
+              <h1 className='text-lg font-bold'>{'//NNEGARA'}</h1>
+            </Link>
+          </motion.div>
+          
+          <motion.div
+            initial={{
+              x: 200,
+              opacity: 0,
+            }}
+            animate={{
+              x: 0,
+              opacity: 1,
+            }}
+            transition={{
+              duration: 0.3
+            }}
+          >
+            <ModeToggle />
+          </motion.div>
 
-      <ModeToggle />
-    </div>
+        </div>
+      </Container>
+    </header>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,22 @@
 import Nav from "@/components/layout/nav"
+import Image from "next/image"
+
+import Profile from "@/assets/ProfileNN-BW.png"
 
 export default function Home() {
   return (
-    <main className='flex flex-col p-24'>
+    <div className="flex flex-col overflow-x-hidden">
       <Nav />
-
-      <section className="py-24 flex flex-col items-center text-center">
-        <h1 className="text-4xl font-bold">Coming Really Soon!</h1>
-        <p className="text-2xl">Upgraded NNEGARA Portfolio</p>
-      </section>
-    </main>
+      <main className='flex flex-col p-4'>
+        <section className="py-24 h-[calc(100vh-121px)] justify-center flex flex-col items-center text-center">
+          <h1 className="text-4xl font-bold">Coming Really Soon!</h1>
+          <p className="text-2xl mt-2 text-muted-foreground">Upgrading NNEGARA Portfolio</p>
+        </section>
+        {/* <Image
+          alt="profile"
+          src={Profile}
+        /> */}
+      </main>
+    </div>
   )
 }
